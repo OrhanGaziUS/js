b5bddb5b6c832b8af6ca9210b5e63e14c1a65daa
OrhanGaziUS <45637438+OrhanGaziUS@users.noreply.github.com>
2023-10-16 23:33:20 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+function artHome1() {
+  const skor = document.getElementById("ctn");
+  const skorHome = document.getElementById("home");
+  skorHome.innerHTML = parseInt(skorHome.innerHTML) + 1;
+}
+
+function artHome2() {
+  const skorHome = document.getElementById("home");
+  skorHome.innerHTML = parseInt(skorHome.innerHTML) + 2;
+}
+
+function artHome3() {
+  const skorHome = document.getElementById("home");
+  skorHome.innerHTML = parseInt(skorHome.innerHTML) + 3;
+}
+
+function artGuest1() {
+  const skorGuest = document.getElementById("guest");
+  skorGuest.innerHTML = parseInt(skorGuest.innerHTML) + 1;
+}
+function artGuest2() {
+  const skorGuest = document.getElementById("guest");
+  skorGuest.innerHTML = parseInt(skorGuest.innerHTML) + 1;
+}
+function artGuest3() {
+  const skorGuest = document.getElementById("guest");
+  skorGuest.innerHTML = parseInt(skorGuest.innerHTML) + 1;
+}
